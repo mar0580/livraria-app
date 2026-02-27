313b0a40f309c4caa14accdf1511f43029e0ce1a
LuisMarcelo <mar0580@gmail.com>
2026-02-27 15:39:38 -0300

    Refatoração: adição de suporte para favicon com nova rota e arquivo SVG

--- src/main/java/com/livraria/controller/HomeController.java
+++ src/main/java/com/livraria/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.livraria.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +11,11 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/favicon.ico")
+    @ResponseBody
+    public ResponseEntity<Void> favicon() {
+        return ResponseEntity.noContent().build();
     }
 }
